cc6b23eecbdfa1f52b5cfd90972938c5b95b4870
Purvak Lapsiya <purvak.l@somaiya.edu>
2019-01-19 09:06:49 -0800

    Update deep_ranking.py
    Resolved dimension's issue.
    Added normalize and combine(cat) method to merge layers.

--- deep_ranking.py
+++ deep_ranking.py
@@ -10,7 +10,6 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from torchsummary import summary
 
-#For testing using IMG_URL
 IMG_URL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'
 response = requests.get(IMG_URL)
 img = Image.open(io.BytesIO(response.content))  # Read bytes and store as an img.
@@ -27,43 +26,31 @@ img = transform_pipeline(img)
 img = img.unsqueeze(0)  # Insert the new axis at index 0 i.e. in front of the other axes/dims. 
 img = Variable(img)
 
-model = models.vgg16(pretrained=True)  # This may take a few minutes.
-new_classifier = nn.Sequential(*list(model.classifier.children())[:-1])
-model.classifier = new_classifier
 
-# prediction = vgg(img)  # Returns a Tensor of shape (batch, num class labels)
-#prediction = prediction.data.numpy().argmax()  # Our prediction will be the index of the class label with the largest value.
-#vgg.add_module('fin',module=nn.Linear(20,4000))
-
-# x = model(img)
-# x = nn.AvgPool2d(x)
-# print(x)
-# x = F.relu(x)
-# print(x)
-# t = F.normalize(x,p=2, dim=1)
-# first_conv = nn.Conv2d(in_channels=3,out_channels=96,kernel_size=8,stride=16,padding=0)
-# first_max = nn.MaxPool2d(kernel_size=8,stride=4,padding=0)
-
-model = models.vgg16(pretrained=True)  # This may take a few minutes.
-new_classifier = nn.Sequential(*list(model.classifier.children())[:-1])
-model.classifier = new_classifier
+model = models.vgg16(pretrained=True)  
+# new_classifier = nn.Sequential(*list(model.classifier.children())[:-1])
+# model.classifier = new_classifier
+print(summary(model,input_size=(3,224,224)))
 class Network(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3,96,8, stride=16,padding=1)
         self.maxpool1 = nn.MaxPool2d(3,4,padding=1)
         self.conv2 = nn.Conv2d(3,96,8, stride=32,padding=1)
-        self.maxpool2 = nn.MaxPool2d(7,2,padding=1)
+        self.maxpool2 = nn.MaxPool2d(7,2,padding=3)
         
     def forward(self,x):
         out1 = model(x)
         y = self.conv1(x)
         y = self.maxpool1(y)
-        #y = F.normalize(y,dim=1,p=2)
+        y = y.view(y.size(0), -1)
+        y = F.normalize(y,dim=1,p=2)
         z = self.conv2(x)
         z = self.maxpool2(z)
-        #z = F.normalize(z,dim=1,p=2)
-        z = emb(z)
+        z = z.view(y.size(0), -1)
+        z = F.normalize(z,dim=1,p=2)
+        #out = torch.cat((out1,y,z),1)
+        out = torch.cat((out1, y, z), 1)
         return out
 test = Network()
 print(summary(test,input_size=(3,224,224)))
