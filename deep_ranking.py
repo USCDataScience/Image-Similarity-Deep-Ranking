be726ad093ae08cf97096819e3f76de829101efe
Purvak Lapsiya <purvak.l@somaiya.edu>
2019-01-25 14:09:47 -0800

    Update deep_ranking.py
    -Modified P network
    - Corrected dimensions of maxpool and conv layer
    - Added Optimizer
    - Added Loss

--- deep_ranking.py
+++ deep_ranking.py
@@ -9,6 +9,8 @@ import torchvision.transforms as transforms
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from torchsummary import summary
+import torch.optim as optim
+
 
 IMG_URL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg'
 response = requests.get(IMG_URL)
@@ -30,27 +32,104 @@ img = Variable(img)
 model = models.vgg16(pretrained=True)  
 new_classifier = nn.Sequential(*list(model.classifier.children())[:-1])
 model.classifier = new_classifier
-print(summary(model,input_size=(3,224,224)))
-class Network(nn.Module):
+# print(summary(model,input_size=(3,224,224)))
+class Network_1(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3,96,8, stride=16,padding=1)
-        self.maxpool1 = nn.MaxPool2d(3,4,padding=1)
-        self.conv2 = nn.Conv2d(3,96,8, stride=32,padding=1)
+        self.model = model
+        self.conv1 = nn.Conv2d(3,3,1, stride=4,padding=2)
+        self.conv2 = nn.Conv2d(3,96,8,stride=4,padding=4)
+        self.maxpool1 = nn.MaxPool2d(3,4,padding=0)
+        self.conv3 = nn.Conv2d(3,3,1, stride=8,padding=2)
+        self.conv4 = nn.Conv2d(3,96,8,stride=4,padding=4)
         self.maxpool2 = nn.MaxPool2d(7,2,padding=3)
-        
     def forward(self,x):
-        out1 = model(x)
-        y = self.conv1(x)
+        out1 = self.model(x)
+        y = self.conv1(x)        
+        y = self.conv2(y)
         y = self.maxpool1(y)
         y = y.view(y.size(0), -1)
         y = F.normalize(y,dim=1,p=2)
-        z = self.conv2(x)
+        z = self.conv3(x)
+        z = self.conv4(z)
         z = self.maxpool2(z)
         z = z.view(y.size(0), -1)
         z = F.normalize(z,dim=1,p=2)
-        #out = torch.cat((out1,y,z),1)
         out = torch.cat((out1, y, z), 1)
-        return out
-test = Network()
-print(summary(test,input_size=(3,224,224)))
+        out = F.normalize(out,dim=1,p=2)
+        return outtest = Network()
+test1 = Network_1()
+print(summary(test1,input_size=(3,224,224)))
+
+# specify loss function (categorical cross-entropy)
+criterion = nn.TripletMarginLoss()
+# specify optimizer
+optimizer = optim.SGD(test1.parameters(), lr=0.01,momentum=0.9,nesterov=True)
+
+#Uncomment after writing trainloader to load data.
+
+'''
+n_epochs = 30
+n_epochs = 30
+
+valid_loss_min = np.Inf # track change in validation loss
+
+for epoch in range(1, n_epochs+1):
+
+    # keep track of training and validation loss
+    train_loss = 0.0
+    valid_loss = 0.0
+    
+    ###################
+    # train the model #
+    ###################
+    model.train()
+    for data, target in train_loader:
+        # move tensors to GPU if CUDA is available
+        if train_on_gpu:
+            data, target = data.cuda(), target.cuda()
+        # clear the gradients of all optimized variables
+        optimizer.zero_grad()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the batch loss
+        loss = criterion(output, target)
+        # backward pass: compute gradient of the loss with respect to model parameters
+        loss.backward()
+        # perform a single optimization step (parameter update)
+        optimizer.step()
+        # update training loss
+        train_loss += loss.item()*data.size(0)
+        
+    ######################    
+    # validate the model #
+    ######################
+    model.eval()
+    for data, target in valid_loader:
+        # move tensors to GPU if CUDA is available
+        if train_on_gpu:
+            data, target = data.cuda(), target.cuda()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = model(data)
+        # calculate the batch loss
+        loss = criterion(output, target)
+        # update average validation loss 
+        valid_loss += loss.item()*data.size(0)
+    
+    # calculate average losses
+    train_loss = train_loss/len(train_loader.dataset)
+    valid_loss = valid_loss/len(valid_loader.dataset)
+        
+    # print training/validation statistics 
+    print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
+        epoch, train_loss, valid_loss))
+    
+    # save model if validation loss has decreased
+    if valid_loss <= valid_loss_min:
+        print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(
+        valid_loss_min,
+        valid_loss))
+        torch.save(model.state_dict(), 'model.pt')
+        valid_loss_min = valid_loss
+
+'''
