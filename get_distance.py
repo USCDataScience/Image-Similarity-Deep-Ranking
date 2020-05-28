9075f0c72214dc2468c960138475ec7586644d29
Purvak Lapsiya <purvak.l@somaiya.edu>
2020-05-27 19:51:59 -0700

    Update get_distance.py
    Solved import error

--- get_distance.py
+++ get_distance.py
@@ -40,6 +40,8 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Embedding
+from keras import backend as K
+
 
 def convnet_model_():
     vgg_model = VGG16(weights=None, include_top=False)
